a0537a73af78f95915bb6e6ea5d05532da09ca40
lilikavitadia <lilikavitadia@apps.ipb.ac.id>
2026-03-04 04:55:00 +0700

    add db setup script

---
+++ connection/db_setup.R
@@ -0,0 +1,31 @@
+# =========================================================
+# db_setup.R - Setup Database Film (Revisi 3NF)
+# Kelompok 5 - Role: Database Manager
+# DB: MariaDB/MySQL (DBngin)
+# =========================================================
+
+library(DBI)
+library(RMariaDB)
+
+csv_path <- "data/raw/Dataset Film Raw.csv"
+
+db_config <- list(
+  host = "127.0.0.1",
+  port = 3307,
+  user = "root",
+  password = "",
+  dbname = "db_bioskop"
+)
+
+con <- dbConnect(RMariaDB::MariaDB(),
+                 user = db_config$user,
+                 password = db_config$password,
+                 host = db_config$host,
+                 port = db_config$port)
+
+on.exit(try(dbDisconnect(con), silent = TRUE), add = TRUE)
+
+dbExecute(con, paste("CREATE DATABASE IF NOT EXISTS", db_config$dbname))
+dbExecute(con, paste("USE", db_config$dbname))
+
+cat("Connected OK. Run connection/ddl.sql to create tables.\n")
